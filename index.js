2d0dfcfbbfa648047fab94e7db222fd9ce6be329
Yurii <yurii.kryvko@gmail.com>
2020-10-03 21:28:48 +0300

    portfolio link

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 console.log('hello, again! i\'m back!')
+const hamburger = document.getElementById('hamburger-icon');
+const menuBox = document.querySelector('.menu__box');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const homeTab = document.querySelector('.home');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  hamburger.onclick = (function() {
+     hamburger.classList.toggle('active');
+     menuBox.style.visibility = menuBox.style.visibility === 'visible' ? '' : 'visible';
+     return false;
+  });
+
+});
+
+menuBox.addEventListener('click', (event) => {
+
+  const tabs = document.querySelectorAll('.menu__item');
+  if (event.target && event.target.classList.contains('menu__item')) {
+    tabs.forEach((item, i) => {
+      if (event.target == item) {
+        console.log('das')
+        hamburger.classList.toggle('active');
+        menuBox.style.visibility = menuBox.style.visibility === 'visible' ? '' : 'visible';
+      }
+    })
+  }
+});
 
 document.querySelector('.tabheader').addEventListener('click', (event) => {
   const tabs = document.querySelectorAll('.tabs-item');
   if (event.target && event.target.classList.contains('tabs-item')) {
-    console.log('contains tab')
     tabs.forEach((item, i) => {
       if (event.target == item) {
         item.classList.add('tabheader__item_active');
@@ -15,14 +42,7 @@ document.querySelector('.tabheader').addEventListener('click', (event) => {
 });
 
 
-const menuBox = document.querySelector('.menu__box');
-menuBox.addEventListener('click', (event) => {
-  const sideBarTabs = document.querySelectorAll('.menu__item');
-  if (event.target && event.target.classList.contains('menu__item')) {
-    sideBarTabs.forEach((item, i) => {
-      if (event.target == item) {
-        menuBox.style.visibility = 'visible';
-      }
-    })
-  }
-})
+
+
+
+console.log(window.pageYOffset)

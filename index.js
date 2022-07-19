ae59617524be9219af5272f14d0579e3d1cf4032
Yyyurii <yurii.kryvko@gmail.com>
2022-07-19 18:31:37 +0300

    Added favicon

--- index.js
+++ index.js
@@ -1,76 +1,75 @@
-console.log('hello, again! i\'m back!')
-const hamburger = document.getElementById('hamburger-icon');
-const menuBox = document.querySelector('.menu__box');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const homeTab = document.querySelector('.home');
-const tabs = document.querySelectorAll('.tabs-item');
+const hamburger = document.getElementById("hamburger-icon");
+const menuBox = document.querySelector(".menu__box");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const homeTab = document.querySelector(".home");
+const tabs = document.querySelectorAll(".tabs-item");
 
+document.addEventListener("DOMContentLoaded", function () {
+  homeTab.classList.add("tabheader__item_active");
 
-document.addEventListener('DOMContentLoaded', function () {
-  homeTab.classList.add('tabheader__item_active');
-
-  hamburger.onclick = (function () {
-    hamburger.classList.toggle('active');
-    menuBox.style.visibility = menuBox.style.visibility === 'visible' ? '' : 'visible';
+  hamburger.onclick = function () {
+    hamburger.classList.toggle("active");
+    menuBox.style.visibility =
+      menuBox.style.visibility === "visible" ? "" : "visible";
     return false;
-  });
+  };
 });
 
-
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   if (window.pageYOffset === 0) {
-    homeTab.classList.add('tabheader__item_active');
+    homeTab.classList.add("tabheader__item_active");
   } else {
     tabs.forEach((item, i) => {
-      item.classList.remove('tabheader__item_active');
-    })
+      item.classList.remove("tabheader__item_active");
+    });
   }
 });
 
-
-menuBox.addEventListener('click', (event) => {
-  const tabs = document.querySelectorAll('.menu__item');
-  if (event.target && event.target.classList.contains('menu__item')) {
+menuBox.addEventListener("click", (event) => {
+  const tabs = document.querySelectorAll(".menu__item");
+  if (event.target && event.target.classList.contains("menu__item")) {
     tabs.forEach((item, i) => {
       if (event.target == item) {
-        console.log('das')
-        hamburger.classList.toggle('active');
-        menuBox.style.visibility = menuBox.style.visibility === 'visible' ? '' : 'visible';
+        console.log("das");
+        hamburger.classList.toggle("active");
+        menuBox.style.visibility =
+          menuBox.style.visibility === "visible" ? "" : "visible";
       }
-    })
+    });
   }
 });
 
-
-document.querySelector('.tabheader').addEventListener('click', (event) => {
-  if (event.target && event.target.classList.contains('tabs-item')) {
+document.querySelector(".tabheader").addEventListener("click", (event) => {
+  if (event.target && event.target.classList.contains("tabs-item")) {
     tabs.forEach((item, i) => {
       if (event.target == item) {
-        item.classList.add('tabheader__item_active');
+        item.classList.add("tabheader__item_active");
       } else {
-        item.classList.remove('tabheader__item_active');
+        item.classList.remove("tabheader__item_active");
       }
-    })
+    });
   }
 });
 
-document.querySelector('.btn').addEventListener('click', () => {
-  console.log('mail')
-  window.location = document.getElementById('mail-link').href;
-})
+document.querySelector(".btn").addEventListener("click", () => {
+  console.log("mail");
+  window.location = document.getElementById("mail-link").href;
+});
 
-$(function() {
-  $('.scrollup').click(function() {
-    $("html, body").animate({
-      scrollTop:0
-    },800);
-  })
-})
-$(window).scroll(function() {
-  if ($(this).scrollTop()>200) {
-    $('.scrollup').fadeIn();
-  }
-  else {
-    $('.scrollup').fadeOut();
+$(function () {
+  $(".scrollup").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      800
+    );
+  });
+});
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 200) {
+    $(".scrollup").fadeIn();
+  } else {
+    $(".scrollup").fadeOut();
   }
 });
